4d16e05f239f65b621fc68d649ed50a0d10ead97
Hugovalpo <lillo.hugo@gmail.com>
2023-09-25 12:27:19 +0200

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import style from "../styles/Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PhoneIcon from '@mui/icons-material/Phone';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-// import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
@@ -13,15 +13,15 @@ export default function Footer() {
             <span>TIENDITECH</span>
           </div>
 
-          <div>
-            <span>
-              <PhoneIcon className={style.icon} />
+          <div >
+            <span >
+              <FontAwesomeIcon icon={faPhone} className={style.icon} />
               +55 55 55 55 55
             </span>
 
-            <span>
-              <MailOutlineIcon className={style.icon} />
-              example@tienditech.com
+            <span >
+              <FontAwesomeIcon icon={faEnvelope} className={style.icon}/>
+               example@tienditech.com
             </span>
           </div>
         </div>
